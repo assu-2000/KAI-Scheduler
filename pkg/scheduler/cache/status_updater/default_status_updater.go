36332ea091f0814641491838c2648c4a10264bab
davidLif <davidshani12@gmail.com>
2025-06-16 18:36:47 +0300

    Do not create error events for successfully scheduled podGroups - v0.5 (#237)

--- pkg/scheduler/cache/status_updater/default_status_updater.go
+++ pkg/scheduler/cache/status_updater/default_status_updater.go
@@ -190,16 +190,19 @@ func (su *defaultStatusUpdater) RecordJobStatusEvent(job *podgroup_info.PodGroup
 		su.recordStaleJobEvent(job)
 	}
 
-	if !job.IsReadyForScheduling() {
-		su.recordJobNotReadyEvent(job)
-		return nil
-	}
+	updatePodgroupStatus := false
+	if job.GetNumPendingTasks() > 0 || job.GetNumGatedTasks() > 0 {
+		if !job.IsReadyForScheduling() {
+			su.recordJobNotReadyEvent(job)
+			return nil
+		}
 
-	if err := su.recordUnschedulablePodsEvents(job); err != nil {
-		return err
-	}
+		if err := su.recordUnschedulablePodsEvents(job); err != nil {
+			return err
+		}
 
-	updatePodgroupStatus := su.recordUnschedulablePodGroup(job)
+		updatePodgroupStatus = su.recordUnschedulablePodGroup(job)
+	}
 
 	if len(patchData) > 0 || updatePodgroupStatus {
 		su.pushToUpdateQueue(
